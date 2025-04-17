21960fa7c4fe5ab4f1009368872ae015114cd3e8
DaNamesXeodon <xeodon7@gmail.com>
2025-04-17 22:42:42 +0100

    Update main.py

--- Spambot/main.py
+++ Spambot/main.py
@@ -3,35 +3,36 @@ import time
 import pyKey
 import pyautogui
 
-# Find the path of you
-whatPath = "C:\\Program Files\\WindowsApps\\5319275A.WhatsAppDesktop_2.2514.4.0_x64__cv1g1gvanyjgm\\WhatsApp.exe"
+def sendMessage(whatPath:str, msg:str, dest:str, reps:int = 1):
+    """
+    Send a whatsapp Message.
+    args:
+        whatPath (str): Path of the WhatsApp executable
+        msg (str): Message to be sent
+        dest (str): Group or contact name to send to
+        reps (int): Number of times the message should be sent
+    """
+    # Run WhatsApp
+    os.startfile(whatPath)
+    time.sleep(5)
 
-# Get the message to be sent, the destination, and the number of times it should be sent
-msg = input("What message would you like to send?\n")
-dest = input("Who would you like to send the message to?\n")
-reps = int(input("How many times do you want the message to be sent?\n"))
-
-# Run WhatsApp
-os.startfile(whatPath)
-time.sleep(5)
-
-# Search for the group and open it, and the message typing bar
-for l in dest:
-    k = "SPACEBAR" if l == " " else l
-    pyKey.press(k, 0.01)
-time.sleep(1.5)
-pyautogui.click(240, 190)
-time.sleep(1)
-pyautogui.click(1200, 1005)
-time.sleep(1)
-
-# Send message(s)
-for rep in range(0, reps):
-    for l in msg:
+    # Search for the group and open it, and the message typing bar
+    for l in dest:
         k = "SPACEBAR" if l == " " else l
         pyKey.press(k, 0.01)
-    pyKey.press("ENTER", 0.01)
-    time.sleep(0.2)
-
-# Close WhatsApp
-os.system(f"taskkill /f /im WhatsApp.exe")
+    time.sleep(1.5)
+    pyautogui.click(240, 190)
+    time.sleep(1)
+    pyautogui.click(1200, 1005)
+    time.sleep(1)
+    
+    # Send message(s)
+    for rep in range(0, reps):
+        for l in msg:
+            k = "SPACEBAR" if l == " " else l
+            pyKey.press(k, 0.01)
+        pyKey.press("ENTER", 0.01)
+        time.sleep(0.2)
+    
+    # Close WhatsApp
+    os.system(f"taskkill /f /im WhatsApp.exe")
